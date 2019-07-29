ddf13733ba93be98012e4f681f18b0a24b6f6909
Anastasiia <nastiykr@gmail.com>
2019-07-29 23:48:36 +0300

    Autotest added

--- test_items.py
+++ test_items.py
@@ -4,5 +4,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     browser.find_element_by_css_selector("button.btn-add-to-basket")
